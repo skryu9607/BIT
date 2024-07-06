8505e3da34d0ea3bab85b2f1395064c6aceaf7f3
m1nm0rn1n6 <156638998+m1nm0rn1n6@users.noreply.github.com>
2024-07-07 00:53:19 +0900

    1. windmap size (map_x, map_y) => (x_min, x_max, y_min, y_max) 2. 가장 가까운 좌표의 데이터를 추출하도록 수정

--- wind_catcher.py
+++ wind_catcher.py
@@ -1,3 +1,8 @@
+"""
+Thermal updrafts modeling
+@author : Minjo Jung
+"""
+
 import numpy as np
 
 def wind_catcher(x,y,z,u,v,w):
@@ -7,35 +12,29 @@ def wind_catcher(x,y,z,u,v,w):
     # y : -map_x ~ map_x
     # z : resolution ~ 0.9*zi
 
-    map_x = 3000
-    map_y = 3000
+    x_min = -1000
+    x_max = 5000
+    y_min = -1000
+    y_max = 5000
+    zi = 4000
     resolution = 10
-    
-    idx_x = int((x + map_x) / resolution)
-    idx_y = int((y + map_y) / resolution)
-    idx_z = int((z - resolution) / resolution)
 
-    wind = [u[idx_x,idx_y,idx_z], v[idx_x,idx_y,idx_z], w[idx_x,idx_y,idx_z]]
+    if x > x_max or x < x_min or y > y_max or y < y_min or z < resolution or z > 0.9*zi:
+        raise ValueError
+    
+    idx_x = round((x - x_min) / resolution)
+    idx_y = round((y - y_min) / resolution)
+    idx_z = round((z - resolution) / resolution)
+
+    # u, v 가 2차원 데이터
+    wind = [u[idx_x,idx_y], v[idx_x,idx_y], w[idx_x,idx_y,idx_z]]
 
     return wind
 
+'''LOAD wind map data : 파일 주소는 알맞게 변경'''
+u = np.load('/Users/minmorning/Documents/Ignatius/ACC_2025/wind/windmap_240707/u.npy')
+v = np.load('/Users/minmorning/Documents/Ignatius/ACC_2025/wind/windmap_240707/v.npy')
+w = np.load('/Users/minmorning/Documents/Ignatius/ACC_2025/wind/windmap_240707/w.npy')
 
-u = np.load('C:/Users/seung/WindData/u.npy')
-v = np.load('C:/Users/seung/WindData/v.npy')
-w = np.load('C:/Users/seung/WindData/w.npy')
-
-print(type(u[0,0]))
-# print(u)
-# print(u.shape)
-# print(u[0,0,50])
-
-# print(v)
-# print(v.shape)
-# print(v[0,0,50])
-
-# print(w)
-# print(w.shape)
-# print(w[0,0,50])
-
-wind = wind_catcher(-500,-400,899,u,v,w)
+wind = wind_catcher(-1000,5000,3600,u,v,w)
 print(wind)
