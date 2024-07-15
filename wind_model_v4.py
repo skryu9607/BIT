58b9ad7f6d49cef5a3725615f5e48740cb516466
Seung <skryu9607@kaist.ac.kr>
2024-07-15 16:02:20 +0900

    slight change

--- wind_model_v4.py
+++ wind_model_v4.py
@@ -202,7 +202,7 @@ x_len = x_max - x_min
 y_len = y_max - y_min
 
 # Resolution 설정 (개수)
-ResolutionType = 'coarse'
+ResolutionType = 'high'
 # Coarse 
 if ResolutionType == 'coarse':
     resolution = 20
@@ -276,7 +276,22 @@ t.toc()
 np.save(f'C:/Users/seung/WindData/u_{ResolutionType}', u)
 np.save(f'C:/Users/seung/WindData/v_{ResolutionType}', v)
 np.save(f'C:/Users/seung/WindData/w_{ResolutionType}', w)
-        
+'''
+# OneDrive 경로 설정
+onedrive_path = 'C:/Users/seung/OneDrive/문서/WindData/'
+
+# 파일 저장 경로 설정
+u_file_path = f'{onedrive_path}u_coarse.npy'
+v_file_path = f'{onedrive_path}v_coarse.npy'
+w_file_path = f'{onedrive_path}w_coarse.npy'
+
+# 데이터 저장
+np.save(u_file_path, data)
+np.save(v_file_path, data)
+np.save(w_file_path, data)
+
+print("파일이 OneDrive에 저장되었습니다.")
+'''      
 # # 3D Surface Plot in a specific altitude 
 # fig = plt.figure()
 # ax = fig.add_subplot(111, projection='3d')
